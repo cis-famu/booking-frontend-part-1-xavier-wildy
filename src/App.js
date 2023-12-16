916adddaf433879d298c2e108f9c9e60bec11fff
xavier-wildy <79477645+xavier-wildy@users.noreply.github.com>
2023-12-16 18:42:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from "./Pages/Home";
+import React from "react";
+import Home from "./Pages/Home";
+import Search from "./Pages/Search";
+import Booking from "./Pages/Booking";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/search" element={<Search />} />
+                    <Route path="/booking" element={<Booking />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
